9463c78b438b341c031ef63c60d4305118b81955
Densanity <denniszixiang@gmail.com>
2026-02-25 20:49:08 +0100

    Update CharactersPage.vue

--- src/views/CharactersPage.vue
+++ src/views/CharactersPage.vue
@@ -160,24 +160,6 @@
         <LoadingMoreSpinner />
       </div>
     </div>
-    <div
-      class="fixed bottom-4 sm:bottom-6 right-4 sm:right-6 btn btn-soft btn-info px-3 sm:px-4"
-      @click="scrollToTop"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        class="bi bi-arrow-up"
-        viewBox="0 0 16 16"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"
-        />
-      </svg>
-    </div>
   </div>
 </template>
 
