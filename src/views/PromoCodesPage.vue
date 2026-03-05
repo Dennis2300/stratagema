3141c38be6c72407bad1a572f84377ae145c3312
Densanity <denniszixiang@gmail.com>
2026-03-05 20:28:47 +0100

    Update PromoCodesPage.vue

--- src/views/PromoCodesPage.vue
+++ src/views/PromoCodesPage.vue
@@ -1,5 +1,8 @@
 <template>
-  <section>
+  <section class="mt-2 flex flex-col justify-center items-center">
     <h1>All Available Genshin Codes</h1>
+    <div>
+        
+    </div>
   </section>
 </template>
