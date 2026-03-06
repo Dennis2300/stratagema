160b5f6652efb4fb8a34270fc3653e13a2e42975
Densanity <denniszixiang@gmail.com>
2026-03-06 23:02:53 +0100

    refactored page layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <NavBar />
-  <main class="pt-28 md:pt-20 min-h-screen">
-    <router-view />
-  </main>
-  <Footer />
+  <div class="drawer lg:drawer-open">
+    <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+      <!-- Navbar -->
+      <NavBar />
+      <!-- Page content here -->
+      <div class="p-4 mt-12">
+        <router-view />
+      </div>
+      <Footer />
+    </div>
+    <!-- Drawer Sidebar -->
+    <SidebarDrawer />
+  </div>
   <Cookies />
   <SpeedInsights />
 </template>
@@ -14,6 +23,7 @@ import { SpeedInsights } from "@vercel/speed-insights/vue";
 import NavBar from "./components/Page/NavBar.vue";
 import Footer from "./components/Page/Footer.vue";
 import Cookies from "./components/Page/Cookies.vue";
+import SidebarDrawer from "./components/Page/SidebarDrawer.vue";
 // Initialize Vercel Analytics
 inject();
 </script>
